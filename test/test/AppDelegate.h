bdcbf3c1d9e7e85a896ea8c5e1b3aea479d66407
Suger <928991159@qq.com>
2016-04-15 21:54:58 +0800

    it just  a test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by baichenggang on 16/4/15.
+//  Copyright © 2016年 Sugar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
