778742e4d8bedfceb502651d3b211296bb00bf02
Suger <928991159@qq.com>
2016-04-18 14:55:31 +0800

    it is only a group

---
+++ ged/ged/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ged
+//
+//  Created by baichenggang on 16/4/18.
+//  Copyright © 2016年 Sugar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
